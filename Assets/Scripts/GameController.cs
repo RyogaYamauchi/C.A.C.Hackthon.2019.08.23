5800350d57f0d47c11987cf8d6d47794039d8bc5
RyogaYamauchi <g1854003@cc.kyoto-su.ac.jp>
2019-08-28 00:12:06 +0900

    タイムとスコアついか

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private TimeView _timeView;
+    [SerializeField] private ScoreView _scoreView;
+
+    public int Time = 0;
+    public int Score = 0;
+    void Start()
+    {
+        StartCoroutine(CountDownTime());
+    }
+
+    public IEnumerator CountDownTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1.0f);
+            Time++;
+            Updatetime();
+        }
+    }
+
+    private void AdditionalScore(int score)
+    {
+        Score += score;
+        UpdateScore();
+    }
+
+    private void UpdateScore()
+    {
+        _scoreView.SetScoreText(Score.ToString());
+    }
+
+    private void Updatetime()
+    {
+        _timeView.Updatetime(Time);
+    }
+        
+}
---
+++ Assets/Scripts/TimeView.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    public class TimeView : MonoBehaviour
+    {
+        [SerializeField] private Text Text;
+
+        public void Updatetime(int num)
+        {
+            int big = num / 60;
+            int small = num % 60;
+            Text.text = $"{big}:{small}";
+        }
+    }
+}

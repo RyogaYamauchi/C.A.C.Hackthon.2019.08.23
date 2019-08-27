5c2f17991e7e1a415157e4346695394ad5de30b9
RyogaYamauchi <g1854003@cc.kyoto-su.ac.jp>
2019-08-28 00:59:06 +0900

    5秒に１回タピオカが流れてくるように

---
+++ Assets/Scripts/DestoryWithTime4.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class DestoryWithTime4 : MonoBehaviour
+    {
+        private int time = 0;
+        private int limitTime = 4;
+        private void Start()
+        {
+            StartCoroutine(Count10time());
+        }
+
+        private IEnumerator Count10time()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(1.0f);
+                time++;
+                if (time > limitTime)
+                {
+                    gameObject.SetActive(false);
+                    yield break;
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,9 +7,13 @@ public class GameController : MonoBehaviour
 {
     [SerializeField] private TimeView _timeView;
     [SerializeField] private ScoreView _scoreView;
+    [SerializeField] private Transform SpawnPoint;
+
+    public List<GameObject> PlayerHoldingItems = new List<GameObject>();
 
     public int Time = 0;
     public int Score = 0;
+    
     void Start()
     {
         StartCoroutine(CountDownTime());
@@ -22,7 +26,17 @@ public class GameController : MonoBehaviour
             yield return new WaitForSeconds(1.0f);
             Time++;
             Updatetime();
+            if (Time % 5 ==0)
+            {
+                SpawnTapioka();
+            }
         }
+    }
+
+    private void SpawnTapioka()
+    {
+        var obj = Resources.LoadAsync("Tapioka");
+        var instance = (GameObject) Instantiate(obj.asset, SpawnPoint.position, Quaternion.identity);
     }
 
     private void AdditionalScore(int score)
